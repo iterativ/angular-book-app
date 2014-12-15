1129b6b7252cff336abc66b6a60012d2e257ce10
Daniel Egger <daniel.egger@gmail.com>
2014-12-15 17:54:34 +0100

    cache data on article service

--- app/scripts/article/article.service.js
+++ app/scripts/article/article.service.js
@@ -5,15 +5,24 @@ angular.module('itApp.article').factory('articleService', articleService);
 articleService.$inject = ['$q', '$http'];
 
 function articleService($q, $http) {
+  var cachedData = null;
+
   return {
     getArticles: getArticles,
     getArticleById: getArticleById
   };
 
   function getArticles() {
-    return $http.get('/scripts/article/articledata.json').then(function(response) {
-      return response.data;
-    });
+    if(cachedData) {
+      return $q.when(cachedData);
+    }
+    else {
+      return $http.get('/scripts/article/articledata.json').then(function(response) {
+        // store the data in the cache variable
+        cachedData = response.data;
+        return cachedData;
+      });
+    }
   }
 
   function getArticleById(id) {
