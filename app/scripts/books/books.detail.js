a0d3cde2cd5a1925be641d84995bbb287ecda650
Pawel Kowalski <pawel.kowalski@iterativ.ch>
2015-07-01 16:23:38 +0200

    uncomment sending sentry messages

--- app/scripts/books/books.detail.js
+++ app/scripts/books/books.detail.js
@@ -3,19 +3,23 @@
 angular.module('itApp.books').controller('BookDetailController', BookDetailController);
 
 function BookDetailController($log, book, $raven) {
-  var vm = this;
-  vm.book = book;
-  vm.buy = buy;
+    var vm = this;
+    vm.book = book;
+    vm.buy = buy;
 
-  activate();
+    activate();
 
-  function activate() {
-    $log.debug('BookDetailController activated');
-    $raven.captureMessage('BookDetailController activated');
-  }
+    function activate() {
+        $log.debug('BookDetailController activated');
 
-  function buy() {
-    $log.debug('Download this Book');
-    $amazon.download('asdasd');
-  }
+        // TODO: un-uncomment to see how messages are sent to sentry
+        //$raven.captureMessage('BookDetailController activated');
+    }
+
+    function buy() {
+        $log.debug('Download this Book');
+
+        // TODO: un-uncomment to see how problems are captured with sentry
+        //$amazon.download('asdasd');
+    }
 }
