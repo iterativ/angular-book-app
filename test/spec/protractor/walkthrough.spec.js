92eb6760964e57cdd6e47524bdb2ecc275f7b91f
Pawel Kowalski <pawel.kowalski@iterativ.ch>
2015-07-07 14:54:10 +0200

    re-add

--- test/spec/protractor/walkthrough.spec.js
+++ test/spec/protractor/walkthrough.spec.js
@@ -1,35 +1,35 @@
 describe('In walkthroug trough the Best-Practice App a user can', function() {
 
-    //it('see the start page', function() {
-    //    browser.get('/');
-    //    var logo = element(by.css('a.navbar-brand'));
-    //    logo.click().then(function() {
-    //        var title = element(by.tagName('h1'));
-    //        expect(title.getText()).toBe('Best Practices Demo App');
-    //    });
-    //});
-    //
-    //it('click on about menu', function() {
-    //    browser.get('/');
-    //    var menuLinks = element(by.css('ul.navbar-nav')).all(by.tagName('a'));
-    //    var link = menuLinks.get(0);
-    //    expect(link.getText()).toBe('About');
-    //    link.click().then(function() {
-    //        var title = element(by.tagName('h1'));
-    //        expect(title.getText()).toBe('About');
-    //    });
-    //});
-    //
-    //it('click on books menu', function() {
-    //    browser.get('/');
-    //    var menuLinks = element(by.css('ul.navbar-nav')).all(by.tagName('a'));
-    //    var link = menuLinks.get(1);
-    //    expect(link.getText()).toBe('Books');
-    //    link.click().then(function() {
-    //        var title = element(by.tagName('h1'));
-    //        expect(title.getText()).toBe('Books');
-    //    });
-    //});
+    it('see the start page', function() {
+        browser.get('/');
+        var logo = element(by.css('a.navbar-brand'));
+        logo.click().then(function() {
+            var title = element(by.tagName('h1'));
+            expect(title.getText()).toBe('Best Practices Demo App');
+        });
+    });
+
+    it('click on about menu', function() {
+        browser.get('/');
+        var menuLinks = element(by.css('ul.navbar-nav')).all(by.tagName('a'));
+        var link = menuLinks.get(0);
+        expect(link.getText()).toBe('About');
+        link.click().then(function() {
+            var title = element(by.tagName('h1'));
+            expect(title.getText()).toBe('About');
+        });
+    });
+
+    it('click on books menu', function() {
+        browser.get('/');
+        var menuLinks = element(by.css('ul.navbar-nav')).all(by.tagName('a'));
+        var link = menuLinks.get(1);
+        expect(link.getText()).toBe('Books');
+        link.click().then(function() {
+            var title = element(by.tagName('h1'));
+            expect(title.getText()).toBe('Books');
+        });
+    });
 
     it('click on examples menu', function() {
         browser.get('/');
@@ -69,18 +69,18 @@ describe('In walkthroug trough the Best-Practice App a user can', function() {
         });
     });
 
-    //it('click on examples menu', function() {
-    //    browser.get('/');
-    //    var menuLinks = element(by.css('ul.navbar-nav')).all(by.tagName('a'));
-    //    var link = menuLinks.get(2);
-    //    expect(link.getText()).toBe('Examples');
-    //    link.click().then(function() {
-    //
-    //        //browser.pause();
-    //
-    //        var title = element(by.tagName('h1'));
-    //        expect(title.getText()).toBe('Example');
-    //    });
-    //});
+    it('click on examples menu', function() {
+        browser.get('/');
+        var menuLinks = element(by.css('ul.navbar-nav')).all(by.tagName('a'));
+        var link = menuLinks.get(2);
+        expect(link.getText()).toBe('Examples');
+        link.click().then(function() {
+
+            //browser.pause();
+
+            var title = element(by.tagName('h1'));
+            expect(title.getText()).toBe('Example');
+        });
+    });
 
 });
